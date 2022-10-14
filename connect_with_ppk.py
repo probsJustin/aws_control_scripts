8fea219238733223bbd9ec1f83f393d172f75972
Justin Hagerty <wheelsmanx@gmail.com>
2022-10-14 11:46:44 -0400

    added ssh connect with ppk

---
+++ connect_with_ppk.py
@@ -0,0 +1,10 @@
+import paramiko
+ssh = paramiko.SSHClient()
+
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh.connect('<hostname>', username='<username>', password='<password>', key_filename='<path/to/openssh-private-key-file>')
+
+stdin, stdout, stderr = ssh.exec_command('ls')
+print(stdout.readlines())
+ssh.close()
